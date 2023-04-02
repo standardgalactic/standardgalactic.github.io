a2d3430058c5f8ffa3b32b5bdae15ee9c3820ad2
Cogito Ergo Sum <standardgalactic@protonmail.com>
2023-04-02 16:47:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,92 @@
-var canvas = document.getElementById("canvas");
+// Get the canvas element and context
+var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
 
-var stars = [];
+// Game objects
+var player = {
+    x: canvas.width/2,
+    y: canvas.height-50,
+    speed: 5,
+    size: 50
+};
+var bullets = [];
+var enemies = [];
 
-// Generate stars
-for (var i = 0; i < 100; i++) {
-  var star = {
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    size: Math.random() * 3,
-  };
-  stars.push(star);
+// Movement functions
+function moveLeft() {
+    player.x -= player.speed;
+}
+function moveRight() {
+    player.x += player.speed;
 }
 
-// Move the stars
-function moveStars(dx, dy) {
-  for (var i = 0; i < stars.length; i++) {
-    stars[i].x += dx;
-    stars[i].y += dy;
+// Enemy creation function
+function createEnemy() {
+    var enemy = {
+        x: Math.floor(Math.random() * (canvas.width-50)),
+        y: 0,
+        speed: Math.floor(Math.random() * 5) + 1,
+        size: 50,
+        letter: getRandomWord()
+    };
+    enemies.push(enemy);
+}
 
-    // Wrap around the screen
-    if (stars[i].x < 0) {
-      stars[i].x = canvas.width;
-    } else if (stars[i].x > canvas.width) {
-      stars[i].x = 0;
+// Collision detection functions
+function checkBulletEnemyCollision(bullet, enemy) {
+    if (bullet.x < enemy.x + enemy.size && 
+        bullet.x + bullet.size > enemy.x && 
+        bullet.y < enemy.y + enemy.size && 
+        bullet.y + bullet.size > enemy.y) {
+        return true;
     }
-    if (stars[i].y < 0) {
-      stars[i].y = canvas.height;
-    } else if (stars[i].y > canvas.height) {
-      stars[i].y = 0;
+    return false;
+}
+function checkEnemyPlayerCollision(enemy) {
+    if (enemy.x < player.x + player.size && 
+        enemy.x + enemy.size > player.x && 
+        enemy.y < player.y + player.size && 
+        enemy.y + enemy.size > player.y) {
+        return true;
     }
-  }
+    return false;
 }
 
-// Draw the stars
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Draw the stars
-  ctx.fillStyle = "white";
-  for (var i = 0; i < stars.length; i++) {
-    ctx.beginPath();
-    ctx.arc(stars[i].x, stars[i].y, stars[i].size, 0, 2 * Math.PI);
-    ctx.fill();
-  }
+// Scoring system
+var score = 0;
+function updateScore() {
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("Score: " + score, 10, 50);
 }
 
-// Move the stars when a key is pressed
-document.addEventListener("keydown", function (event) {
-  if (event.key === "a") {
-    moveStars(-5, 0);
-  } else if (event.key === "d") {
-    moveStars(5, 0);
-  } else if (event.key === "w") {
-    moveStars(0, -5);
-  } else if (event.key === "s") {
-    moveStars(0, 5);
-  }
+// Sound effects
+var bulletSound = new Audio("bullet.wav");
+var enemySound = new Audio("enemy.wav");
+
+// Word generation
+var words = ["APPLE", "BANANA", "CHERRY", "DATES", "EGGPLANT", "FIGS", "GRAPE", "HONEYDEW", "JACKFRUIT", "KIWI", "LEMON", "MANGO", "NECTARINE", "ORANGE", "PINEAPPLE", "QUINCE", "RASPBERRY", "STRAWBERRY", "TOMATO", "WATERMELON"];
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+// Keyboard input handling
+var input = "";
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        // If a letter key is pressed, add it to the input string
+        input += String.fromCharCode(event.keyCode);
+    } else if (event.keyCode == 8) {
+        // If the backspace key is pressed, remove the last character from the input string
+        input = input.slice(0, -1);
+    }
 });
 
-// Start the game loop
-setInterval(function () {
-  draw();
-}, 30);
+// Game loop
+function gameLoop() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // Draw the player
+    ctx.fillStyle = "green";
+    ctx.fillRect(player.x, player.y, player.size, player.size);
