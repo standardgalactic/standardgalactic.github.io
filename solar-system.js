cbe0f33c916291ab19a420649a07474b08b5173d
standardgalactic <standardgalactic@protonmail.com>
2025-03-09 16:33:30 -0300

    Vorticon Planet
    Not working yet.
    
    Attempting to merge starfield (index.html) and typing-tutor
    (typing-tutor.html) into a single game but having some issues.
    
    https://standardgalactic.github.io/vorticon-planet.html

---
+++ solar-system.js
@@ -0,0 +1,18 @@
+// This file exports an array of planet names.
+// You can change these names or even localize them for different languages or game themes.
+
+const planetNames = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune",
+  "Pluto",
+  "Ceres",
+  "Eris",
+  "Haumea",
+  "Makemake"
+];
